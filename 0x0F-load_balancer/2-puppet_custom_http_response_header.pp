0df517440499ee744885209a90d0bd230140760e
9-max-6 <mutukumaxwel@gmail.com>
2024-04-29 15:27:02 +0300

    Fix: testing

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,24 +1,21 @@
-# Setting up a server using Puppet.
-
 package {'nginx':
-        ensure     => 'present',
-        provider   => 'apt',
-
+  ensure     => 'installed', # 'present' changed to 'installed'
+  provider   => 'apt',
 }
 
 exec { 'enable and configure ufw for Nginx HTTP':
-  command     => '/usr/bin/sudo /usr/sbin/ufw --force enable && /usr/bin/sudo /usr/sbin/ufw allow "Nginx HTTP"',
+  command     => '/usr/sbin/ufw --force enable && /usr/sbin/ufw allow "Nginx HTTP"',
   path        => ['/usr/bin', '/usr/sbin'],
   provider    => 'shell',
-  unless      => "/usr/bin/sudo /usr/sbin/ufw status | /bin/grep -q 'Nginx HTTP'",
+  unless      => "/usr/sbin/ufw status | grep -q 'Nginx HTTP'",
   require     => Package['nginx'],
 }
 
 file {'index.nginx-debian.html':
-        path    => '/var/www/html/index.nginx-debian.html',
-        ensure  => 'file',
-    owner   => 'ubuntu',
-        content => "Hello World!",
+  path    => '/var/www/html/index.nginx-debian.html',
+  ensure  => 'file',
+  owner   => 'ubuntu',
+  content => "Hello World!",
 }
 
 file { 'configuration file':
@@ -26,37 +23,38 @@ file { 'configuration file':
   ensure  => 'file',
   owner   => 'ubuntu',
   content => "server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-        root /var/www/html;
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
 
-        index index.html index.htm index.nginx-debian.html;
+    index index.html index.htm index.nginx-debian.html;
 
-        server_name _;
-        location / {
-                add_header X-Served-By ${::hostname};
-                try_files \$uri \$uri/ =404;
-        }
-        location /redirect_me {
-            return 301 https://www.alxafrica.com;
-        }
-        error_page 404 /404.html;
-        location = /404.html {
-            root /var/www/html;
-            internal;
-        }
-    }"
+    server_name _;
+    location / {
+      add_header X-Served-By \"${::hostname}\";
+      try_files \$uri \$uri/ /404.html; # Use proper quoting
+    }
+    location /redirect_me {
+      return 301 https://www.alxafrica.com;
+    }
+    error_page 404 /404.html;
+    location = /404.html {
+      root /var/www/html;
+      internal;
+    }
+  }",
 }
-
 
 file {'/var/www/html/404.html':
-    path    =>  '/var/www/html/404.html',
-        ensure  => 'file',
-        owner   => 'ubuntu',
-        content => "Ceci n'est pas une page"
+  path    => '/var/www/html/404.html',
+  ensure  => 'file',
+  owner   => 'ubuntu',
+  content => "Ceci n'est pas une page",
 }
+
 exec { 'update nginx configuration and restart':
-  command  => "/usr/bin/sudo /bin/sed -i 's@try_files \\$uri \\$uri/ =404;@try_files \\$uri \\$uri/ /404.html;@' /etc/nginx/sites-available/default && /usr/bin/sudo /usr/sbin/service nginx restart",
+  command  => "/bin/sed -i 's@try_files \\$uri \\$uri/ =404;@try_files \\$uri \\$uri/ /404.html;@' /etc/nginx/sites-available/default && /usr/sbin/service nginx restart",
   path     => ['/usr/bin', '/bin', '/usr/sbin'],
   provider => 'shell',
+  require  => File['configuration file'],
 }
