c233abda10821f6d9910e54e9d93b257296cede3
9-max-6 <mutukumaxwel@gmail.com>
2024-06-05 15:49:12 +0300

    Chore: new task

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Recursive function
+"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None, count=0):
+    """A function that queries the Reddit API and returns
+    a list containing the titles of all hot articles for a given subreddit"""
+    if after is None and count != 0:
+        return hot_list
+    if after is not None:
+        url = "https://www.reddit.com/r/{}/.json?after={}".format(
+            subreddit, after
+            )
+    else:
+        url = "https://www.reddit.com/r/{}/.json".format(subreddit)
+
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 100
+    }
+    with requests.get(url, headers=headers, params=params,
+                      allow_redirects=False) as resp:
+        if resp.status_code != 200:
+            return hot_list
+        results = resp.json().get("data")
+        for result in results.get('children'):
+            data = result.get('data')
+            hot_list.append(data.get('title'))
+        count += len(results.get('children'))
+        after = results.get('after')
+        return recurse(subreddit, hot_list, after, count)
