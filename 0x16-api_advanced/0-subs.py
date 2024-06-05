19ab88dbecd1128df02e60001623d56600af670a
9-max-6 <mutukumaxwel@gmail.com>
2024-06-05 14:13:47 +0300

    Fix: changed the file to be adherent to PEP8 standards

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,12 +3,13 @@
  """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """A function to determine the number of subscribers
     to a subreddit"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {
-        'User-Agent':'Mozilla/5.0 (X11; Ubuntu; \
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; \
         Linux x86_64; rv:126.0) Gecko/20100101 Firefox/126.0'
     }
     with requests.get(url, headers=headers) as resp:
