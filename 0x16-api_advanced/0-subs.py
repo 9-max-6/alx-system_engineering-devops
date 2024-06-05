2fc549c36a5e2012d94839d75424a92ee60f00b5
9-max-6 <mutukumaxwel@gmail.com>
2024-06-05 14:20:01 +0300

    Fix: should be working now

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,10 +12,10 @@ def number_of_subscribers(subreddit):
         'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; \
         Linux x86_64; rv:126.0) Gecko/20100101 Firefox/126.0'
     }
-    with requests.get(url, headers=headers) as resp:
+    with requests.get(url, headers=headers, allow_redirects=False) as resp:
         if resp.status_code != 200:
             return 0
         else:
-            resp_data = resp.json()['data']
+            resp_data = resp.json().get('data')
             number_of_subscribers = resp_data.get('subscribers')
             return number_of_subscribers
